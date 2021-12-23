54b2380f602dc86f31e6c32245b96609a2773a36
karthik19-cloud <32713545+karthik19-cloud@users.noreply.github.com>
2021-12-23 13:43:42 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -6,13 +6,7 @@ importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compa
 
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
-  apiKey: "AIzaSyDUEHyPATBWEuH5fp-_GVd-KwseSafWI7c",
-  authDomain: "weather-461c1.firebaseapp.com",
-  databaseURL: "https://weather-461c1.firebaseio.com",
-  projectId: "weather-461c1",
-  storageBucket: "weather-461c1.appspot.com",
-  messagingSenderId: "1043412625214",
-  appId: "1:1043412625214:web:7841637689d2af00f7ee6f"
+  
 };
 
 // eslint-disable-next-line no-undef
